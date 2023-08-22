210c025a01e419097dcd4a07043daca8518265ec
Jéssica Cristina <jessicacristina.recife@gmail.com>
2023-08-21 22:05:12 -0300

    Update meuexercicio27.py

--- exercicios/para-casa/meuexercicio27.py
+++ exercicios/para-casa/meuexercicio27.py
@@ -20,16 +20,16 @@ maca_maior_5kg = 1.5
 kg_morango = float(input('Digite a quantidade em kg de morangos: '))
 kg_maca = float(input('Digite a quantidade em kg de maçãs: '))
 
-if kg_morango < 5:
+if kg_morango <= 5:
     preco_morango = kg_morango * morango_menor_5kg
     print(f'Preço dos morangos R$: {preco_morango:.2f}')
-elif kg_morango >= 5:
+elif kg_morango > 5:
     preco_morango = kg_morango * morango_maior_5kg
     print(f'Preço dos morangos R$: {preco_morango:.2f}')
-if kg_maca < 5:
+if kg_maca <= 5:
     preco_maca = kg_maca * maca_menor_5kg
     print(f'Preço das maçãs R$: {preco_maca:.2f}')
-elif kg_maca >= 5:
+elif kg_maca > 5:
     preco_maca = kg_maca * maca_maior_5kg
     print(f'Preço das maçãs R$: {preco_maca:.2f}')
 
